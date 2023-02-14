48497ac06fb551b90cc5add14a121936abaf9d8d
Ru Chern Chong <hello@ruchern.xyz>
2023-02-14 22:44:13 +0800

    Add Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import ViteRadar from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteRadar({
+      analytics: {
+        id: "G-4W2DF7BF1S",
+      },
+    }),
+  ],
+});
