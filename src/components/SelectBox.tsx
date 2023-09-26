65d086c56460d0dae195b8af6441f94430320e39
Ru Chern Chong <hello@ruchern.xyz>
2023-09-27 00:52:37 +0800

    Add accessibility label

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -12,15 +12,17 @@ export const SelectBox = (props: SelectBoxProps) => {
   const { name, id, defaultValue, children, onChange, ...rest } = props;
 
   return (
-    <select
-      name={name}
-      id={id}
-      className="mb-2 w-full cursor-pointer appearance-none rounded-lg p-2 text-neutral-900 md:w-1/3"
-      defaultValue={defaultValue}
-      onChange={onChange}
-      {...rest}
-    >
-      {children}
-    </select>
+    <label htmlFor={id}>
+      <select
+        name={name}
+        id={id}
+        className="mb-2 w-full cursor-pointer appearance-none rounded-lg p-2 text-neutral-900 md:w-1/3"
+        defaultValue={defaultValue}
+        onChange={onChange}
+        {...rest}
+      >
+        {children}
+      </select>
+    </label>
   );
 };
