aacc176c7f1a2a971c7b37ddaa71515c3f13ee98
Ru Chern Chong <hello@ruchern.xyz>
2023-11-18 16:24:31 +0800

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,32 @@
+import { PropsWithChildren } from "react";
+
+interface LinkProps extends PropsWithChildren {
+  href: string;
+}
+
+const Link = ({ href, children }: LinkProps) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener"
+      className="text-neutral-400 underline"
+    >
+      {children}
+    </a>
+  );
+};
+
 export const Footer = () => {
   return (
     <footer className="flex flex-col items-center bg-neutral-800 p-4">
       <div>
-        Created by{" "}
-        <a
-          href="https://ruchern.xyz"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-neutral-400 underline"
-        >
-          Chong Ru Chern
-        </a>
+        Created by <Link href="https://ruchern.xyz">Ru Chern</Link>
       </div>
       <div>
-        <a
-          href="https://github.com/ruchernchong/cpf-calculator"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-neutral-400 underline"
-        >
+        <Link href="https://github.com/ruchernchong/cpf-contribution-calculator">
           Source code
-        </a>{" "}
+        </Link>{" "}
         on GitHub
       </div>
     </footer>
