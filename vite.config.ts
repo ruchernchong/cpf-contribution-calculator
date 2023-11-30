d2b00680b760f808a08fb1c277b5ac86eec96972
Ru Chern Chong <hello@ruchern.xyz>
2023-12-01 02:28:10 +0800

    Update theme colour for PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
         name: title,
         short_name: title,
         description,
-        theme_color: "#000000",
+        theme_color: "#0d9488",
         icons: [
           {
             src: "pwa-64x64.png",
