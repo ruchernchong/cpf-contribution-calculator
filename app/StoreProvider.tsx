85c2f4ca350bd34a54c236fcb74a531ee468304d
Ru Chern Chong <hello@ruchern.xyz>
2024-01-01 05:39:54 +0800

    Fix build errors from redux config

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "../lib/store";
 
 const StoreProvider = ({ children }: { children: React.ReactNode }) => {
--- lib/store.ts
+++ lib/store.ts
@@ -1,11 +1,21 @@
-import { combineReducers, configureStore, createStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import dataReducer from "./features/data/dataSlice";
 
 const reducers = combineReducers({
   data: dataReducer,
 });
+
 const persistConfig = {
   key: "root",
   storage,
@@ -14,11 +24,19 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
-export const store = createStore(persistedReducer);
+// export const store = createStore(persistedReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
 export const persistor = persistStore(store);
 
-// Infer the type of makeStore
-// export type AppStore = ReturnType<typeof store>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<AppStore["getState"]>;
-// export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
