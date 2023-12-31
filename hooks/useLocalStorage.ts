8d7770eac977ac4d9ccda954cd9b408921f795d2
Ru Chern Chong <hello@ruchern.xyz>
2024-01-01 02:18:47 +0800

    Add type instead of any to useLocalStorage hook

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
-export const useLocalStorage = (key: string, initialValue: any) => {
-  const [value, setValue] = useState(() => {
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [value, setValue] = useState<T>(() => {
     if (typeof window !== "undefined") {
       const storedValue = window.localStorage.getItem(key);
       return storedValue ? JSON.parse(storedValue) : initialValue;
