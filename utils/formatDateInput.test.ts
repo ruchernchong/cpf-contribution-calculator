a44c645128c12376dfb1fc445dcd3a06c166b164
Ru Chern Chong <hello@ruchern.xyz>
2024-01-30 00:14:40 +0800

    Clean up function

---
+++ utils/formatDateInput.test.ts
@@ -0,0 +1,7 @@
+import { formatDateInput } from "./formatDateInput";
+
+describe("formatDateInput", () => {
+  it("should return the date in the correct format", () => {
+    expect(formatDateInput("012024", "012024")).toBe("01/2024");
+  });
+});
--- utils/formatDateInput.ts
+++ utils/formatDateInput.ts
@@ -1,4 +1,14 @@
-export const formatDateInput = (rawInput: string, birthDate: string) => {
+/**
+ * Automatically format the birthdate from MMYYYY to MM/YYYY
+ * e.g. 012023 to 01/2023
+ *
+ * @param rawInput
+ * @param birthDate
+ */
+export const formatDateInput = (
+  rawInput: string,
+  birthDate: string,
+): string => {
   if (rawInput.length === 2 && birthDate.length === 3) {
     rawInput = rawInput.slice(0, 1) + "/";
   } else {
