3768ad42d246fe79d519b4bb97243706a7a0d0a2
Ru Chern Chong <hello@ruchern.dev>
2025-10-17 17:54:19 +0800

    style: fix tabs overflow on mobile viewport

--- src/components/navigation-tabs.tsx
+++ src/components/navigation-tabs.tsx
@@ -15,14 +15,16 @@ export const NavigationTabs = () => {
   const pathname = usePathname();
 
   return (
-    <Tabs value={pathname}>
-      <TabsList>
-        {navigationItems.map((item) => (
-          <TabsTrigger key={item.href} value={item.href} asChild>
-            <Link href={item.href}>{item.label}</Link>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-    </Tabs>
+    <div className="overflow-x-auto">
+      <Tabs value={pathname}>
+        <TabsList>
+          {navigationItems.map((item) => (
+            <TabsTrigger key={item.href} value={item.href} asChild>
+              <Link href={item.href}>{item.label}</Link>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+    </div>
   );
 };
